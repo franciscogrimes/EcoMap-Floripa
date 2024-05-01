bf3d4508f0249dac442e4cc0afcf90c33143b431
franciscogrimes <fgsieg1831@gmail.com>
2024-04-30 21:31:10 -0300

    Edições no mapa para deixar ele mais amigável

--- Ecohubs/src/pages/PaginaInicial.jsx
+++ Ecohubs/src/pages/PaginaInicial.jsx
@@ -1,14 +1,38 @@
-import Navbar from "../components/Navbar/Navbar"
+import Navbar from "../components/Navbar/Navbar";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import style from "./styles/PaginaInicial.module.css";
+import Maps from "leaflet";
 
-function home(){
-    
-    return(
-        <>
-        <Navbar/>
-        <h1>Home</h1>
-        </>
-    )
+const markerIcon = new Maps.Icon({
+  iconUrl: require("./assets/been-here-solid-60.png"),
+  iconSize: [35, 45],
+});
 
+function home() {
+  const position = [51.505, -0.09];
+
+  return (
+    <div>
+      <Navbar />
+
+      <MapContainer
+        center={position}
+        zoom={13}
+        className={style.Leaflet_container}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[13.084622, 80.248357]} icon={markerIcon}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
 
-export default home
+export default home;
