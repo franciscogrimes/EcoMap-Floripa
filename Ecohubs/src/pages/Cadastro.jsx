32215aa66d37a624f86f5d4dd3f8cdc3b058b651
franciscogrimes <fgsieg1831@gmail.com>
2024-05-04 22:11:47 -0300

    Estilização do menu de cadastro

--- Ecohubs/src/pages/Cadastro.jsx
+++ Ecohubs/src/pages/Cadastro.jsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import { useContext } from "react";
 import { UtilitsContext } from "../components/context/UtilitsContext";
+import style from "./styles/Cadastro.module.css";
 
 function Cadastro() {
   const { register, setValue, getValues, handleSubmit } = useForm();
@@ -29,23 +30,22 @@ function Cadastro() {
   }
 
   return (
-    <div>
+    <div className={style.container}>
       <form>
-        <div>
-          <h2>Dados pessoais:</h2>
+        <h2>Dados pessoais:</h2>
+        <div className={style.dadosPessoais}>
+          <div className={style.dpCol1}>
+            <label htmlFor="name">Nome Completo: </label>
+            <input
+              type="text"
+              name="name"
+              {...register("name", {
+                required: true,
+                maxLength: 50,
+                minLength: 10,
+              })}
+            />
 
-          <label htmlFor="name">Nome Completo: </label>
-          <input
-            type="text"
-            name="name"
-            {...register("name", {
-              required: true,
-              maxLength: 50,
-              minLength: 10,
-            })}
-          />
-
-          <div>
             <label htmlFor="gender">Gênero:</label>
             <select name="gender" {...register("gender")}>
               <option>Selecione uma opção</option>
@@ -53,59 +53,63 @@ function Cadastro() {
               <option value="male">Masculino</option>
               <option value="other">Outro</option>
             </select>
+            <label htmlFor="cpf">CPF:</label>
+            <input
+              type="number"
+              name="cpf"
+              {...register("cpf", {
+                maxLength: 11,
+                minLength: 11,
+              })}
+            />
           </div>
-          <label htmlFor="cpf">CPF:</label>
-          <input
-            type="number"
-            name="cpf"
-            {...register("cpf", {
-              maxLength: 11,
-              minLength: 11,
-            })}
-          />
-          <label htmlFor="birth">Data de nascimento:</label>
-          <input type="date" name="birth" />
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            name="email"
-            {...register("email", {
-              required: true,
-            })}
-          />
-          <label htmlFor="password">Senha:</label>
-          <input
-            type="password"
-            name="password"
-            {...register("password", {})}
-          />
+          <div className={style.dpCol2}>
+            <label htmlFor="birth">Data de nascimento:</label>
+            <input type="date" name="birth" />
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              {...register("email", {
+                required: true,
+              })}
+            />
+            <label htmlFor="password">Senha:</label>
+            <input
+              type="password"
+              name="password"
+              {...register("password", {})}
+            />
+          </div>
         </div>
-        <div>
-          <h2>Endereço:</h2>
+        <h2>Endereço:</h2>
+        <div className={style.endereco}>
+          <div className={style.endCol1}>
+            <label htmlFor="cep">CEP:</label>
+            <input
+              type="text"
+              name="cep"
+              {...register("cep", {
+                required: true,
+                maxLength: 8,
+                minLength: 8,
+                onBlur: () => enderecoCompleto(),
+              })}
+            />
+            <label htmlFor="neighborhood">Bairro:</label>
+            <input
+              type="text"
+              name="neighborhood"
+              {...register("neighborhood")}
+            />
+          </div>
+          <div className={style.endCol2}>
+            <label htmlFor="city">Cidade:</label>
+            <input type="text" name="city" {...register("city")} />
 
-          <label htmlFor="cep">CEP:</label>
-          <input
-            type="text"
-            name="cep"
-            {...register("cep", {
-              required: true,
-              maxLength: 8,
-              minLength: 8,
-              onBlur: () => enderecoCompleto(),
-            })}
-          />
-          <label htmlFor="neighborhood">Bairro:</label>
-          <input
-            type="text"
-            name="neighborhood"
-            {...register("neighborhood")}
-          />
-
-          <label htmlFor="city">Cidade:</label>
-          <input type="text" name="city" {...register("city")} />
-
-          <label htmlFor="state">Estado:</label>
-          <input type="text" name="state" {...register("state")} />
+            <label htmlFor="state">Estado:</label>
+            <input type="text" name="state" {...register("state")} />
+          </div>
         </div>
         <button onClick={handleSubmit(onSubmit)}>Cadastrar Usuário</button>
       </form>
