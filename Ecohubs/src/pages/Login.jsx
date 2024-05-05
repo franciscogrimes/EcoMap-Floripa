348a1df695e32a1d284d17594eeaff84ec9eefc2
franciscogrimes <fgsieg1831@gmail.com>
2024-05-04 21:23:51 -0300

    Estilização da page de Login

--- Ecohubs/src/pages/Login.jsx
+++ Ecohubs/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import { useContext, useState } from "react";
 import { UtilitsContext } from "../components/context/UtilitsContext";
+import style from "./styles/Login.module.css";
+import { Link } from "react-router-dom";
 
 export default function App() {
   const [usuario, setUsuario] = useState({
@@ -14,26 +16,38 @@ export default function App() {
   }
 
   return (
-    <div>
-      <div>
-        <input
-          type="email"
-          value={usuario.email}
-          placeholder="Email"
-          onChange={(evento) =>
-            setUsuario({ ...usuario, email: evento.target.value })
-          }
-        />
-        <input
-          type="password"
-          value={usuario.senha}
-          placeholder="Senha"
-          onChange={(evento) =>
-            setUsuario({ ...usuario, senha: evento.target.value })
-          }
-        />
+    <div className={style.container}>
+      <div className={style.containerLogin}>
+        <div className={style.title}>
+          <h1>Login</h1>
+        </div>
+        <div className={style.input}>
+          <label htmlFor="email">Email:</label>
+          <input
+            className={style.inputEmail}
+            type="email"
+            value={usuario.email}
+            onChange={(evento) =>
+              setUsuario({ ...usuario, email: evento.target.value })
+            }
+          />
+          <label htmlFor="password">Senha:</label>
+          <input
+            className={style.inputPass}
+            type="password"
+            value={usuario.senha}
+            onChange={(evento) =>
+              setUsuario({ ...usuario, senha: evento.target.value })
+            }
+          />
+        </div>
+        <button onClick={() => validacao()}>Login</button>
+        <div className={style.signUp}>
+          <Link to="/sign-up" relative="path">
+            Ainda não possui cadastro?
+          </Link>
+        </div>
       </div>
-      <button onClick={() => validacao()}></button>
     </div>
   );
 }
